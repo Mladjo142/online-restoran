12432b9137a013d0b84e4dc3bd75bf8b9eb62c0a
antesoldo <89462492+antesoldo@users.noreply.github.com>
2022-10-06 00:16:46 +0200

    Update update.php

--- update.php
+++ update.php
@@ -31,7 +31,7 @@ if (!$konekcija) {
 }
 
 
-$result = $konekcija -> query("UPDATE musterija, korisnik
+$result = $konekcija -> query("UPDATE Musterija, Korisnik
 				SET Ime ='$Ime', Prezime ='$Prezime', BrojTelefona ='$BrTel', Adresa ='$Adresa' 
 				WHERE Musterija.IDKorisnika=Korisnik.ID AND Korisnik.KorisnickoIme='$KorIme'");
 
